b790862dfb1ac0b6d2d6df3a53f62818af5aa09a
prashant48653c <acharyaprashant227@gmail.com>
2025-11-09 17:48:17 +0545

    Changes in cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 // CORS configuration
 const corsOptions = {
-   origin: "*",
+    origin: ["http://localhost:3000", "https://newsadik.vercel.app","https://sadiksha.com.np"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
